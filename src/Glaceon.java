4f17ae3d9e97d679a2a4c1cf9bb3c61ba8d80826
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 20:03:44 -0300

    meu segundo commit

---
+++ src/Glaceon.java
@@ -0,0 +1,16 @@
+public class Glaceon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "gelo cortante";
+    }
+    @Override
+    public String defesa (){
+        return "chuva de gelo";
+    
+    }
+    @Override
+    public String especial(){
+        return "polo norte ";
+    }
+}
