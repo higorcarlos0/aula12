b9840a668c062ca1cc0f377ce6ab2260f33bd7eb
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 19:58:39 -0300

    meu segundo commit

---
+++ src/Floreon.java
@@ -0,0 +1,16 @@
+public class Floreon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "bola de fogo";
+    }
+    @Override
+    public String defesa (){
+        return "parede flamejante";
+    
+    }
+    @Override
+    public String especial(){
+        return "chama furacão ";
+    }
+}
