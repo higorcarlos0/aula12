fc1dbb7bddf199a0ecf15ad161e7fd8b53c993c5
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 20:00:33 -0300

    meu segundo commit

---
+++ src/Vaporeon.java
@@ -0,0 +1,16 @@
+public class Vaporeon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "revolver d'agua";
+    }
+    @Override
+    public String defesa (){
+        return "onda gigante";
+    
+    }
+    @Override
+    public String especial(){
+        return "jato d'agua ";
+    }
+}
