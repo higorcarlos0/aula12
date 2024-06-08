0c5cc548165495fd7da50e247f0ef58389635330
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 19:52:13 -0300

    meu segundo commit

---
+++ src/Umbreon.java
@@ -0,0 +1,16 @@
+public class Umbreon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "Rosnada - ataque rapido";
+    }
+    @Override
+    public String defesa (){
+        return "parede noturna";
+    
+    }
+    @Override
+    public String especial(){
+        return "jogo sujo ";
+    }
+}
