dce157fcb88da5f0e7031d0d56c1f29045f0a932
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 19:45:36 -0300

    meu segundo commit

---
+++ src/Espeon.java
@@ -0,0 +1,16 @@
+public class Espeon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "Onda de veneno";
+    }
+    @Override
+    public String defesa (){
+        return "nuvem paralisante";
+    
+    }
+    @Override
+    public String especial(){
+        return "parade de veneno";
+    }
+}
