ad6dafce332a16b6d49f8f3cb741dfb86827987b
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 20:02:35 -0300

    meu segundo commit

---
+++ src/Leafeon.java
@@ -0,0 +1,16 @@
+public class Leafeon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "folha navalha";
+    }
+    @Override
+    public String defesa (){
+        return "lâmina da folha";
+    
+    }
+    @Override
+    public String especial(){
+        return "floresta suprema ";
+    }
+}
