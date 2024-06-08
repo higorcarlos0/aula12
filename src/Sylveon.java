fc47f403a3b1bc51975833a6ff47599041e051df
higorcarlos0 <161603095+higorcarlos0@users.noreply.github.com>
2024-06-08 19:55:42 -0300

    meu segundo commit

---
+++ src/Sylveon.java
@@ -0,0 +1,16 @@
+public class Sylveon extends Eevee {
+    
+    @Override
+    public String ataque (){
+        return "Clarão desnubrante";
+    }
+    @Override
+    public String defesa (){
+        return "Encanto";
+    
+    }
+    @Override
+    public String especial(){
+        return "nublado encatador ";
+    }
+}
